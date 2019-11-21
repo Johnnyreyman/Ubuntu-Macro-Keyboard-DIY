aa5cd02e0abdf59d04fcfeb24edef88f840b0e13
Johnathan Reynolds <johnathanr@Synolvia-Triumph>
2019-11-20 20:13:25 -0600

    Cleaned up GUI with no wrap.

--- Macro-Keyboard Enabler Ubuntu.sh
+++ Macro-Keyboard Enabler Ubuntu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #This script automates the process of creating a macro-keyboard from any second keyboard using actkbd.
-zenity --info --text "This simple wizard will help you enable your second keyboard to be a macro-keyboard."
+zenity --info --no-wrap --text "This simple wizard will help you enable your second keyboard to be a macro-keyboard."
 #Test=$("command -v actkbd")
 # Check that actkbd exists
 if [ "$(command -v actkbd)" ]
 then
-    zenity --info --text "actkbd is already installed. Click OK to continue."
+    zenity --info --no-wrap --text "actkbd is already installed. Click OK to continue."
 else
-    $(zenity --question --text "Would you like to install 'actkbd' to your computer?")
+    $(zenity --question --no-wrap --text "Would you like to install 'actkbd' to your computer?")
     if [ $? -eq 0 ]
 	then
 		cd /tmp
@@ -17,14 +17,14 @@ else
         cp ../actkbd.config .
 		sudo -i  make install
     else
-        zenity --info --text "Setup cancelled."
+        zenity --info --no-wrap --text "Setup cancelled."
         exit
     fi
-zenity --info --text "Installation of 'actkbd' completed sucessfully."
+zenity --info --no-wrap --text "Installation of 'actkbd' completed sucessfully."
 
 fi
 
-zenity --info --text "Copy the correct device ID for the second keyboard from the list."
+zenity --info --no-wrap --text "Copy the correct device ID for the second keyboard from the list."
 cd /tmp
 xinput list |  xargs -L 20 > Devices.txt
 # Check that leafpad exists
@@ -43,10 +43,10 @@ sudo apt install -y xclip
 fi
 ID="$(xclip -selection c -o)"
 xinput --disable $ID
-zenity --info --text "Input device number $ID is disabled."
-zenity --info --text "Find the name of the correct device and copy the event number next to sysreq."
+zenity --info --no-wrap --text "Input device number $ID is disabled."
+zenity --info --no-wrap --text "Find the name of the correct device and copy the event number next to sysreq."
 cat /proc/bus/input/devices > Inputlist.txt
 leafpad Inputlist.txt
 EVNUM="$(xclip -selection c -o)"
-zenity --info --text "Press all keys on keyboard that will be used. Remember order of buttons pressed. Press 'ctrl+c' when finished."
+zenity --info --no-wrap --text "Press all keys on keyboard that will be used. Remember order of buttons pressed. Press 'ctrl+c' when finished."
 sudo actkbd -s -d /dev/input/event$EVNUM > actkbd.config
